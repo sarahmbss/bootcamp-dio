67aa00786c4a36f508de9bc5705b87d09f41f4ac
Sarah Braga <86424605+sarahmbss@users.noreply.github.com>
2022-11-06 17:46:11 -0300

    Create projeto2-linux.sh

---
+++ cloud-developer/projeto2-linux.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Para script, usa apt-get
+agt-get update 
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+# Baixar o arquivo que deve ser colocar na pasta do apache
+cd /tmp
+wget
+unzip main.zip
+cd linux-site-dio-main
+
+# Copiar todos os arquivos para a pasta padrão do apache 
+cp -R * /var/www/html/
